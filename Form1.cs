0d65fad9b69e25921b1f1d80e79bd5e4365da8d5
Lloza <leonardo.loza.20isc@tecsanpedro.edu.mx>
2022-03-20 01:00:04 -0600

    Cambios: se añadieron mas opciones pero se incluyeron  funciones

--- Form1.cs
+++ Form1.cs
@@ -26,10 +26,10 @@ namespace TiempoEjecucion_LeonardoLoza
             TextBox cajaTextoCURP = new TextBox();
             TextBox cajaTextoDireccion = new TextBox();
             TextBox cajaTextoEstado = new TextBox();
-            TextBox cajaTextoNumeroCorreo = new TextBox();
-            TextBox cajaTextoSemestre = new TextBox();
+            TextBox cajaTextoCorreoElectronico = new TextBox();
             TextBox cajaTextoPromedio = new TextBox();
             TextBox cajaTextoNombreEscuela = new TextBox();
+            TextBox cajaTextoNumeroTelefono = new TextBox();
     
 
 
@@ -52,14 +52,43 @@ namespace TiempoEjecucion_LeonardoLoza
             cajaTextoCURP.Name = "txtBoxCURP";
             cajaTextoCURP.Location = new Point(100, 120);
             cajaTextoCURP.Height = 80;
+            cajaTextoCURP.Width = 120;
+
+            cajaTextoDireccion.Name = "txtBoxDireccion";
+            cajaTextoDireccion.Location = new Point(100, 250);
+            cajaTextoCURP.Height = 80;
+            cajaTextoCURP.Width = 130;
+
+            cajaTextoEstado.Name = "txtEstadoPersona";
+            cajaTextoEstado.Location = new Point(100, 280);
+            cajaTextoCURP.Height = 80;
             cajaTextoCURP.Width = 100;
 
+            cajaTextoNumeroTelefono.Name = "txtNumeroTelefono";
+            cajaTextoNumeroTelefono.Location = new Point(100, 310);
+            cajaTextoNumeroTelefono.Height = 80;
+            cajaTextoNumeroTelefono.Width = 110;
+
+            cajaTextoCorreoElectronico.Name = "txtCorreoElectronico";
+            cajaTextoCorreoElectronico.Location = new Point(320,30);
+            cajaTextoCorreoElectronico.Height = 80;
+            cajaTextoCorreoElectronico.Width = 140;
+
+            cajaTextoNombreEscuela.Name = "txtNombreEscuela";
+            cajaTextoNombreEscuela.Location = new Point(320,180);
+            cajaTextoNombreEscuela.Height = 80;
+            cajaTextoNombreEscuela.Width = 100;
+          
 
             this.Controls.Add(cajaTextoNombre);
             this.Controls.Add(cajaTexto2AM);
             this.Controls.Add(cajaTextoAP);
             this.Controls.Add(cajaTextoCURP);
-
+            this.Controls.Add(cajaTextoDireccion);
+            this.Controls.Add(cajaTextoEstado);
+            this.Controls.Add(cajaTextoNumeroTelefono);
+            this.Controls.Add(cajaTextoCorreoElectronico);
+            this.Controls.Add(cajaTextoNombreEscuela);
             #endregion
 
             #region Textos
@@ -70,6 +99,43 @@ namespace TiempoEjecucion_LeonardoLoza
             Label CURP = new Label();
             Label Genero = new Label();
             Label EstadoCivil = new Label();
+            Label DireccionPersona = new Label();
+            Label EstadoNacimiento = new Label();
+            Label NumeroTelefono = new Label();
+            Label CorreoElectronico = new Label();
+            Label Promedio = new Label();
+            Label Escuela = new Label();
+
+            Escuela.Name = "lblEscuela";
+            Escuela.Location = new Point(250, 180);
+            Escuela.Text = "Escuela:";
+            Escuela.AutoSize = true;
+
+            Promedio.Name = "lblPromedio";
+            Promedio.Location = new Point(250, 70);
+            Promedio.Text = "Promedio actual:";
+            Promedio.AutoSize = true;
+
+            CorreoElectronico.Name = "lblCorreoElectronico";
+            CorreoElectronico.Location = new Point(250, 30);
+            CorreoElectronico.Text = "Correo \nElectrónico:";
+            CorreoElectronico.AutoSize = true;
+
+            NumeroTelefono.Name = "lblNumeroTelefono";
+            NumeroTelefono.Location = new Point(0, 310);
+            NumeroTelefono.AutoSize = true;
+            NumeroTelefono.Text = "Numero de teléfono:";
+
+
+            EstadoNacimiento.Name = "lblEstadoNacimiento";
+            EstadoNacimiento.Location = new Point(0, 280);
+            EstadoNacimiento.Text = "Estado:";
+
+
+            DireccionPersona.Name = "lblDireccionPersona";
+            DireccionPersona.Location = new Point(0,250);
+            DireccionPersona.Text = "Dirección:";
+
 
             EstadoCivil.Name = "lblEstadoCivil";
             EstadoCivil.Location = new Point(0,200);
@@ -100,6 +166,8 @@ namespace TiempoEjecucion_LeonardoLoza
             ApellidoPaterno.Text = "Apellido Paterno:";
             CURP.Text = "CURP:";
 
+
+
             this.Controls.Add(Nombre);
             this.Controls.Add(ApellidoMaterno);
             this.Controls.Add(ApellidoPaterno);
@@ -107,12 +175,20 @@ namespace TiempoEjecucion_LeonardoLoza
             this.Controls.Add(Indicacion);
             this.Controls.Add(Genero);
             this.Controls.Add(EstadoCivil);
+            this.Controls.Add(DireccionPersona);
+            this.Controls.Add(EstadoNacimiento);
+            this.Controls.Add(NumeroTelefono);
+            this.Controls.Add(CorreoElectronico);
+            this.Controls.Add(Promedio);
+            this.Controls.Add(Escuela);
             #endregion
 
             #region Botones en Círculo
             RadioButton Masculino = new RadioButton();
             RadioButton Femenino = new RadioButton();
             RadioButton OtroGenero = new RadioButton();
+            RadioButton PromedioNormal = new RadioButton();
+            RadioButton PromedioExcelencia = new RadioButton();
 
             Masculino.Name = "rbMasculino";
             Femenino.Name = "rbFemenino";
@@ -126,22 +202,31 @@ namespace TiempoEjecucion_LeonardoLoza
             Femenino.Text = "Femenino";
             OtroGenero.Text = "Otro Género";
 
+            PromedioNormal.Text = "Menor a 95";
+            PromedioExcelencia.Text = "Mayor a 95";
+
+
             Masculino.Location = new Point(100,150);
             Femenino.Location = new Point(200, 150);
             OtroGenero.Location = new Point(300, 150);
+            PromedioNormal.Location = new Point(250,80);
+            PromedioExcelencia.Location = new Point(250,100);
+
 
 
             this.Controls.Add(Masculino);
             this.Controls.Add(Femenino);
             this.Controls.Add(OtroGenero);
+            this.Controls.Add(PromedioNormal);
+            this.Controls.Add(PromedioExcelencia);
 
             #endregion
 
+            #region ComboBox 
 
-            ListView vistaEstadoCivil = new ListView();
+            ComboBox vistaEstadoCivil = new ComboBox();
 
             
-
             vistaEstadoCivil.Height = 40;
             vistaEstadoCivil.Width = 100;
 
@@ -153,8 +238,12 @@ namespace TiempoEjecucion_LeonardoLoza
 
             this.Controls.Add(vistaEstadoCivil);
 
+            #endregion 
+
+
+
         }
 
-        
+
     }
 }
